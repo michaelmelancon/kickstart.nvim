871e870450c5c3244b6c718a728d44a01e2f7b93
Michael Melancon <michaelmelancon@gmail.com>
2024-07-13 13:29:31 -0500

    Added roslyn

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,4 +11,16 @@ return {
       require('lsp_signature').setup(opts)
     end,
   },
+  --[[ {
+    'neubaner/roslyn.nvim',
+    event = 'l'
+    config = function(_, capabilities)
+      require('roslyn').setup {
+        roslyn_version = '4.11.0-3.24307.2+f0a5241acc0a87edc69f342664c9ed1945fcf3dc',
+        capabilities = capabilities,
+        on_attach = function() end,
+      }
+    end,
+  },]]
+  --,
 }
